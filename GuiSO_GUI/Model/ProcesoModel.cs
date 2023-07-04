40c96a16e8ed186f6d4e56eefa14265de911482e
SnowieMischief <97817883+SnowieMischief@users.noreply.github.com>
2023-07-04 15:58:59 -0300

    Añadido todo lo básico, habría que revisar la UI para que quede mas linda y eso, capaz la funcionalidad puede ir en esta view directamente?

---
+++ GuiSO_GUI/Model/ProcesoModel.cs
@@ -0,0 +1,29 @@
+﻿namespace GuiSO_GUI.Model;
+
+public class ProcesoModel
+{
+    public string Nombre { get; set; }
+    public string NombreText => $"Nombre: {this.Nombre}";
+
+    public string Pid { get; set; }
+    public string PidText => $"PID: {this.Pid}";
+
+    public double Cpu { get; set; }
+    public string CpuText => $"CPU: {this.Cpu}%";
+
+    public string Estado { get; set; }
+    public string EstadoText => $"Estado: {this.Estado}";
+
+    public ProcesoModel(string nombre, string pid, double cpu, string estado)
+    {
+        Nombre = nombre;
+        Pid = pid;
+        Cpu = cpu;
+        Estado = estado;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Nombre)}: {Nombre}, {nameof(Pid)}: {Pid}, {nameof(Cpu)}: {Cpu}, {nameof(Estado)}: {Estado}";
+    }
+}
---
+++ GuiSO_GUI/Services/ProcesosService.cs
@@ -0,0 +1,21 @@
+﻿using GuiSO_GUI.Model;
+
+namespace GuiSO_GUI.Services;
+
+public class ProcesosService
+{
+    private List<ProcesoModel> _procesoModels { get; }
+
+    public ProcesosService()
+    {
+        this._procesoModels = new();
+        _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
+        _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
+    }
+    
+    public async Task<List<ProcesoModel>> GetProcesos()
+    {
+        return this._procesoModels;
+    }
+}

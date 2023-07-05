0fadfaa939f9d8f260b67c55d129f3350e69f190
MischievousHex <santiagopanozzoa@outlook.com>
2023-07-05 19:25:16 -0300

    refactors!

--- GuiSO_GUI/Model/ProcesoModel.cs
+++ GuiSO_GUI/Model/ProcesoModel.cs
@@ -1,29 +1,51 @@
-﻿namespace GuiSO_GUI.Model;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class ProcesoModel
+namespace GuiSO_GUI.Model;
+
+public class ProcesoModel : ObservableObject
 {
-    public string Nombre { get; set; }
+    private string nombre;
+    public string Nombre
+    {
+        get => nombre;
+        set => SetProperty(ref nombre, value);
+    }
     public string NombreText => $"Nombre: {this.Nombre}";
 
-    public string Pid { get; set; }
-    public string PidText => $"PID: {this.Pid}";
+    private string pId;
+    public string PId
+    {
+        get => pId;
+        set => SetProperty(ref pId, value);
+    }
+    public string PIdText => $"PID: {this.PId}";
 
-    public double Cpu { get; set; }
+    private double cpu;
+    public double Cpu
+    {
+        get => cpu;
+        set => SetProperty(ref cpu, value);
+    }
     public string CpuText => $"CPU: {this.Cpu}%";
 
-    public string Estado { get; set; }
+    private string estado;
+    public string Estado
+    {
+        get => estado;
+        set => SetProperty(ref estado, value);
+    }
     public string EstadoText => $"Estado: {this.Estado}";
 
     public ProcesoModel(string nombre, string pid, double cpu, string estado)
     {
         Nombre = nombre;
-        Pid = pid;
+        PId = pid;
         Cpu = cpu;
         Estado = estado;
     }
 
     public override string ToString()
     {
-        return $"{nameof(Nombre)}: {Nombre}, {nameof(Pid)}: {Pid}, {nameof(Cpu)}: {Cpu}, {nameof(Estado)}: {Estado}";
+        return $"{nameof(Nombre)}: {Nombre}, {nameof(PId)}: {PId}, {nameof(Cpu)}: {Cpu}, {nameof(Estado)}: {Estado}";
     }
 }

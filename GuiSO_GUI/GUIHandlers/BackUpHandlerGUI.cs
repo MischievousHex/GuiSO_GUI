a1f2ecb4a6fd061604a5e2d788cbe92d9221a18b
MischievousHex <santiagopanozzoa@outlook.com>
2023-07-06 14:56:25 -0300

    TESTEAR PORFA

---
+++ GuiSO_GUI/GUIHandlers/BackUpHandlerGUI.cs
@@ -0,0 +1,23 @@
+﻿using GuiSO_GUI.Services;
+
+namespace GuiSO_GUI.GUIHandlers;
+
+public class BackUpHandlerGUI
+{
+    public static bool MakeBackUp(string userName)
+    {
+        if (!CampBackUpExiste())
+            FileSystemService.MakeDirectory("C:\\CAMP_BACKUP");
+        if (FileSystemService.DirectoryExists($"C:\\Users\\{userName}"))
+        {
+            FileSystemService.CopyDirectory($"C:\\Users\\{userName}", $"C:\\CAMP_BACKUP\\", true);
+        }
+
+        return FileSystemService.DirectoryExists($"C:\\CAMP_BACKUP\\{userName}");
+    }
+
+    private static bool CampBackUpExiste()
+    {
+        return FileSystemService.DirectoryExists("C:\\CAMP_BACKUP");
+    }
+}

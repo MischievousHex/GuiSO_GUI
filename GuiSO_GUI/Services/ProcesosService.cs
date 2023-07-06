5350430ea8979c2ca8e3745e8a70e490ff4d11ff
MischievousHex <santiagopanozzoa@outlook.com>
2023-07-06 10:49:19 -0300

    se puede scrollear ahora

--- GuiSO_GUI/Services/ProcesosService.cs
+++ GuiSO_GUI/Services/ProcesosService.cs
@@ -12,6 +12,21 @@ public class ProcesosService
         _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
         _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
         _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
+        _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
+        _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
+        _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
+        _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso1", "12", 10.52, "Running"));
+        _procesoModels.Add(new ProcesoModel("Proceso2", "23", 5.14, "Waiting"));
+        _procesoModels.Add(new ProcesoModel("Proceso3", "34", 12.23, "Running"));
     }
     
     public async Task<List<ProcesoModel>> GetProcesos()
